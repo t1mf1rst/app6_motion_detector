c03821bf70a0c107d1d148f69a096135faebeba6
temirlan <m.temirlan7272@gmail.com>
2018-07-17 22:36:20 +0600

    store motion time in csv file

---
+++ motion_detector.py
@@ -0,0 +1,69 @@
+import cv2, pandas
+from datetime import datetime
+# variable to store first static background
+first_frame = None
+status_list = [None, None]
+times = [None, None]
+df = pandas.DataFrame(columns=["Start", "End"])
+
+video = cv2.VideoCapture(0)
+while True:
+	check, frame = video.read()
+	# status var tells is there motion
+	status = 0
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# blur image to increase accuracy
+	gray = cv2.GaussianBlur(gray, (21,21), 0)
+
+	# store static background in empty variable on the first iteration of loop
+	if first_frame is None:
+		first_frame = gray
+		continue
+
+	# calculate differensies between first and current images to detect motion
+	delta_frame = cv2.absdiff(first_frame, gray)
+
+	thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+
+	thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
+
+	# find contours of moving object
+	(_, cnts, _) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+	# filterring too small contours
+	for contour in cnts:
+		if cv2.contourArea(contour) < 10000:
+			continue
+		# change status when motion is detected
+		status = 1
+		# draw rectangle around the contour
+		(x, y, w, h) = cv2.boundingRect(contour)
+		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+	status_list.append(status)
+
+	if status_list[-1] == 1 and status_list[-2] == 0:
+		times.append( datetime.now() )
+	if status_list[-1] == 0 and status_list[-2] == 1:
+		times.append( datetime.now() )
+
+	cv2.imshow("Capture frame", gray)
+	cv2.imshow("Delta frame", delta_frame)
+	cv2.imshow("Threshold frame", thresh_frame)
+	cv2.imshow("Colour frame", frame)
+
+	key = cv2.waitKey(1)
+
+	if key == ord('q'):
+		if status == 1:
+			times.append( datetime.now() )
+		break
+
+print( status_list )
+print( times )
+for i in range( 0, len(times), 2 ):
+	df = df.append( {"Start":times[i], "End":times[i+1]}, ignore_index=True )
+
+df.to_csv("Times.csv")
+
+video.release()
+cv2.destroyAllWindows()
